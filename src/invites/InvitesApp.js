0bc3da5ad362a4d86d419088d8694b083826f85c
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-10 13:19:32 -0700

    Created InvitesApp component

---
+++ src/invites/InvitesApp.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class InvitesApp extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <main>
+                <div></div>
+            </main>
+        `;
+    }
+}
+
+export default InvitesApp;
