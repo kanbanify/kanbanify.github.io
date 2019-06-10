5cd477499cccf68e0bcf317c735a1bbe52254dc7
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-10 15:10:37 -0700

    Create invite item for each invite

--- src/invites/InvitesList.js
+++ src/invites/InvitesList.js
@@ -1,10 +1,25 @@
 import Component from '../Component.js';
+import InvitesItem from './InvitesItem.js';
 
 class InvitesList extends Component {
 
+    render() {
+        const dom = this.renderDOM();
+
+        const boards = this.props.boards;
+
+        boards.forEach(board => {
+            const inviteItem = new InvitesItem({ board });
+            dom.appendChild(inviteItem.render());
+        });
+        
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
-            <ul></ul>
+            <ul>
+            </ul>
         `;
     }
 }
