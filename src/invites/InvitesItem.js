79481c04d91a311467603e1a72dc836dbc78326e
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-10 15:56:12 -0700

    Implemented accept and decline invite functionallity

--- src/invites/InvitesItem.js
+++ src/invites/InvitesItem.js
@@ -2,14 +2,34 @@ import Component from '../Component.js';
 
 class InvitesItem extends Component {
 
+    render() {
+        const dom = this.renderDOM();
+        const acceptButton = dom.querySelector('.accept');
+        const declineButton = dom.querySelector('.decline');
+
+        const onAccept = this.props.onAccept;
+        const onDecline = this.props.onDecline;
+        const board = this.props.board;
+
+        declineButton.addEventListener('click', () => {
+            onDecline(board);
+        });
+
+        acceptButton.addEventListener('click', () => {
+            onAccept(board);
+        });
+
+        return dom;
+    }
+
     renderTemplate() {
         const board = this.props.board;
 
         return /*html*/`
             <li>
                 <h2>${board.name}</h2>
-                <button>accept</button>
-                <button>decline</button>
+                <button class="accept">accept</button>
+                <button class="decline">decline</button>
             </li>
         `;
     }
--- src/invites/InvitesList.js
+++ src/invites/InvitesList.js
@@ -1,6 +1,8 @@
 import Component from '../Component.js';
 import InvitesItem from './InvitesItem.js';
 
+import { auth, invitesByUserRef, boardsByUserRef, usersByBoardRef } from '../services/firebase.js';
+
 class InvitesList extends Component {
 
     render() {
@@ -9,8 +11,24 @@ class InvitesList extends Component {
         const boards = this.props.boards;
 
         boards.forEach(board => {
-            const inviteItem = new InvitesItem({ board });
-            dom.appendChild(inviteItem.render());
+            const invitesItem = new InvitesItem({ 
+                board,
+                onDecline: invite => {
+                    console.log(invite.key);
+                    invitesByUserRef.child(auth.currentUser.uid).child(invite.key).remove();
+                },
+                onAccept: invite => {
+                    console.log(invite.key);
+                    boardsByUserRef.child(auth.currentUser.uid).child(invite.key).set({ 
+                        key: invite.key
+                    });
+                    usersByBoardRef.child(invite.key).child(auth.currentUser.uid).set({
+                        key: auth.currentUser.uid
+                    });
+                    invitesByUserRef.child(auth.currentUser.uid).child(invite.key).remove();
+                }
+            });
+            dom.appendChild(invitesItem.render());
         });
         
         return dom;
