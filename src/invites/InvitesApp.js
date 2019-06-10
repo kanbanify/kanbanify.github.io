a72b0704d174e21663e11e5706c2813be9edbcca
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-10 15:01:32 -0700

    created invites app component

--- src/invites/InvitesApp.js
+++ src/invites/InvitesApp.js
@@ -1,12 +1,45 @@
 import Component from '../Component.js';
+import InvitesList from './InvitesList.js';
+import Header from '../shared/Header.js';
+
+import { getBoardsFromKeys } from '../services/actions.js';
+
+import { invitesByUserRef, auth } from '../services/firebase.js';
 
 class InvitesApp extends Component {
 
+    render() {
+        const dom = this.renderDOM();
+
+        const main = dom.querySelector('main');
+
+        const header = new Header({ title: 'Board Invites' });
+        
+        dom.prepend(header.render());
+
+        const invitesList = new InvitesList({ boards: [] });
+
+        invitesByUserRef.child(auth.currentUser.uid).on('value', snapshot => {
+            const boardKeys = snapshot.val() ? Object.values(snapshot.val()) : [];
+            console.log(boardKeys);
+            getBoardsFromKeys(boardKeys).then(boards => {
+                invitesList.update({ boards });
+            });
+            
+        });
+
+        main.appendChild(invitesList.render());
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
-            <main>
-                <div></div>
-            </main>
+            <div>
+                <main>
+                    
+                </main>
+            </div>
         `;
     }
 }
