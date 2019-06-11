c30169c2392ad6228d8bd40190b0d71298c8f5f9
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-11 13:43:47 -0700

    rendered triple dot component

--- src/board/Card.js
+++ src/board/Card.js
@@ -1,6 +1,21 @@
 import Component from '../Component.js';
+import TripleDotButton from '../shared/TripleDotButton.js';
 
 class Card extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        const tripleDotButton = new TripleDotButton({
+            onClick: () => {
+                
+            }
+        });
+
+        dom.appendChild(tripleDotButton.render());
+        
+        return dom;
+    }
 
     renderTemplate() {
         const cardData = this.props.cardData;
