4b9a29a2947f0b9f46ca0bf1dc240d95628be158
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-11 10:37:48 -0700

    Created AddCard component

---
+++ src/board/AddCard.js
@@ -0,0 +1,33 @@
+import Component from '../Component.js';
+
+class AddCard extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const form = dom.querySelector('form');
+        const textArea = dom.querySelector('textArea');
+
+        const onAddCard = this.props.onAddCard;
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            onAddCard(textArea.value);
+            form.reset();
+        });
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <li>
+                <form>
+                    <textarea></textarea>
+                    <button>Add card</button>
+                </form>
+            </li>
+        `;
+    }
+}
+
+export default AddCard;
