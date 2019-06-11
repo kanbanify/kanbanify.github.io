b8c2b20df29576334e9c333a2e78077f479240a5
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-11 10:39:21 -0700

    Exported cardsByList db ref

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -19,3 +19,4 @@ export const boardsByUserRef = db.ref('boardsByUser');
 export const usersByBoardRef = db.ref('usersByBoard');
 export const invitesByUserRef = db.ref('invitesByUser');
 export const listsByBoardRef = db.ref('listsByBoard');
+export const cardsByListRef = db.ref('cardsByList');
