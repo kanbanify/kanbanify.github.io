99c11aaabece2cdcca459f36d56f9ee29917cf0e
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-12 14:16:16 -0700

    created move card menu

--- src/board/MoveCardMenu.js
+++ src/board/MoveCardMenu.js
@@ -1,10 +1,43 @@
 import Component from '../Component.js';
+import MoveListSelect from './MoveListSelect.js';
+import MovePositionSelect from './MovePositonSelect.js';
 
 class MoveCardMenu extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        const card = this.props.card;
+        const list = this.props.list;
+        const lists = this.props.lists;
+
+        const movePositionSelect = new MovePositionSelect({ list, card, ogList: list });
+        dom.prepend(movePositionSelect.render());
+
+        const moveListSelect = new MoveListSelect({ 
+            lists, 
+            list,
+            onListChange: (listKey) => {
+                lists.forEach(childList => {
+                    if(childList.key === listKey) {
+                        movePositionSelect.update({ list: childList, card: {} });
+                    }
+                });
+            }
+        });
+        dom.prepend(moveListSelect.render());
+
+        dom.addEventListener('submit', event => {
+            event.preventDefault();
+        });
+        
+        return dom;
+    }
+    
     renderTemplate() {
         return /*html*/ `
             <form class="move-card-menu">
-                <button>Move</button>
+                <button>Submit</button>
             </form>
         `;
     }
---
+++ src/board/MovePositionOption.js
@@ -0,0 +1,21 @@
+import Component from '../Component.js';
+
+class MovePositonOption extends Component {
+
+    renderTemplate() {
+        const card = this.props.card;
+        const position = this.props.position;
+
+        if(card.position === position) {
+            return /*html*/ `
+            <option selected value=${position}>${position}</option>
+        `;
+        }
+        
+        return /*html*/ `
+            <option value=${position}>${position}</option>
+        `;
+    }
+}
+
+export default MovePositonOption;
