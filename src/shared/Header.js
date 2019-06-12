3cdf70175d7739281b066c6309686ca644d911e2
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-12 16:35:06 -0700

    Added nav to header component and added styles

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -7,6 +7,15 @@ class Header extends Component {
 
     render() {
         const dom = this.renderDOM();
+        const home = dom.querySelector('.home');
+        const invites = dom.querySelector('.invites');
+        const about = dom.querySelector('.about');
+
+        const title = this.props.title;
+
+        title === 'Home' ? home.classList.add('current-page') : '';
+        title === 'Board Invites' ? invites.classList.add('current-page') : '';
+        title === 'About Us' ? about.classList.add('current-page') : '';
 
         auth.onAuthStateChanged((user) => {
             const profile = new Profile({ user });
@@ -17,12 +26,21 @@ class Header extends Component {
     }
 
     renderTemplate() {
-        const title = this.props.title || document.title;
 
         return /*html*/`
             <header>
                 <img class="logo" src="./assets/kanbanify logo.png">
-                <h1>${title}</h1>
+                <ul class="nav">
+                    <a href="./index.html">
+                        <li class="home">Home</li>
+                    </a>
+                    <a href="./invites.html">
+                        <li class="invites">Board Invites</li>
+                    </a>
+                    <a href="./index.html">
+                        <li class="about">About Us</li>
+                    </a>
+                </ul>
             </header>
         `;
     }
