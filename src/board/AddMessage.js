518215a283699c19ebc65b46fb254e8db320a536
Zach Butler <zach.standley93@gmail.com>
2019-06-12 17:47:42 -0700

    made add message component

---
+++ src/board/AddMessage.js
@@ -0,0 +1,30 @@
+import Component from '../Component.js';
+
+class AddMessage extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const input = dom.querySelector('input');
+    
+        const onAddMessage = this.props.onAddMessage;
+
+        dom.addEventListener('submit', event => {
+            event.preventDefault();
+            onAddMessage(input.value);
+            dom.reset();
+        });
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <form class="message-form">
+                <input>
+                <button>Send</button>
+            </form>
+        `;
+    }
+}
+
+export default AddMessage;
