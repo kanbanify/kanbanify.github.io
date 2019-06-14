446721015adbf3d6b75787081da4bbd8d158f4fd
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-14 08:32:21 -0700

    Added basic about us page

---
+++ src/about/AboutContainers.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class AboutContainers extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                
+            </div>
+        `;
+    }
+}
+
+export default AboutContainers;
